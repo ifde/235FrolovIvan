e185010573c0910a0459649efee60bebea0fcd00
ifde <ifde@yandex.ru>
2023-09-19 19:15:54 +0300

    First

---
+++ Module 1/Seminar 4/LastYear/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! \a");
+            Console.WriteLine("ab\bc");
+            Console.WriteLine(3 & 4);
+            Console.WriteLine(-127);
+
+            const byte x = 12, y = 10;
+            int z = 12;
+            sbyte a = (sbyte)(z & y);
+            sbyte t = -12;
+            uint t1 = (uint)t;
+            Console.WriteLine(t1);
+        }
+    }
+}
