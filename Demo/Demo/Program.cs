af9c7595ad7b9e34b7af8626086cc9121d59308d
ifde <ifde@yandex.ru>
2023-09-26 07:37:48 +0300

    First

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Дисциплина: "Программирование"
+ Группа:      БПИ235(2)
+ Студент:     Фролов Иван Григорьевич
+ Задача:      ###
+   Дата:      ##.##.2023
+*/
+
+namespace Demogit 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                // input your code here ...
+
+                Console.WriteLine("Для завершения программы нажмите ESC");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        /// <summary>
+        /// Function description
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        static int Function(int a)
+        {
+            // input your code here ...
+        }
+    }
+}
