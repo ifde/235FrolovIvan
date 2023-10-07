e0112fabf535b400d352c03ae4dd331414c8c251
ifde <ifde@yandex.ru>
2023-10-07 09:53:13 +0300

    First

---
+++ Module 1/Week5/Seminar10/Self03/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Задача:      Self03
+  Дата:      07.10.2023
+*/
+
+//Напишите метод для нахождения максимального
+//элемента в одномерном массиве. Метод должен возвращать
+//значение false, если переданный массив пуст или равен или
+//true при удачном нахождении максимума.
+//Сам максимальный элемент должен сохраняться в виде out-
+//параметра, при возврате false ему присваивается 0
+//В основной программе организуйте ввод одномерного количества
+//– первым указывается количество элементов, а затем сами
+//элементы. После этого найдите максимальный элемент с
+//помощью реализованного метода.
+//Обеспечьте проверку некорректных данных (в том числе на
+//отрицательный размер массива).
+
+using System;
+
+namespace Self03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // повтор решения 
+            do
+            {
+                Console.Clear();
+
+                int n; // длина массива
+                int[] a; // массив целых чисел длины n
+                Console.WriteLine("Введите размер массива:");
+                // вводим длину массива A
+                while (!int.TryParse(Console.ReadLine(), out n) || n < 0)
+                {
+                    Console.WriteLine("Wrong input.");
+                    Console.WriteLine("Введите размер массива:");
+                }
+                a = new int[n];
+
+                Console.WriteLine($"Введите последовательно {n} элементов массива:");
+                for (int i = 0; i < n; i++)
+                {
+                    int temp; // очередной элемент массива
+                    while (!int.TryParse(Console.ReadLine(), out temp))
+                    {
+                        Console.WriteLine("Wrong input. Try again.");
+                    }
+                    a[i] = temp;
+                }
+                int maxValue; // масимальный элемент массива
+                if (TryFindMaxElement(a, out maxValue)) Console.WriteLine($"Масксимальный элемент массива равен {maxValue}");
+                else Console.WriteLine($"Массив пустой");
+
+                Console.WriteLine("\n\n-------------\nНажмите ESC для завершения программы.\nДля повтора нажмите любую другую клавишу.\n-------------");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        public static bool TryFindMaxElement(int[] array, out int maxValue)
+        {
+            maxValue = 0;
+            if (array.Length == 0) return false;
+            maxValue = array[0];
+            foreach (int elem in array)
+            {
+                if (maxValue < elem) maxValue = elem;
+            }
+            return true;
+        }
+    }
+}
---
+++ Module 1/Week5/Seminar10/Self04/Program.cs
@@ -0,0 +1,113 @@
+﻿//Напишите метод, получающий на вход ссылки на два
+//массива целых чисел и добавляющий все чётные элементы первого
+//массива в конец второго. Если первый массив null или пустой,
+//второй массив не должен меняться, а если второй массив пустой или null
+//– по необходимости выделите память для хранения элементов.
+
+using System.Linq.Expressions;
+
+namespace Self04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // повтор решения 
+            do
+            {
+                Console.Clear();
+
+                int[] a, b; // массивы целых чисел
+                Console.WriteLine("\nЗаполните массив A:");
+                Fill_Array(out a);
+                Console.WriteLine("\nЗаполните массив B:");
+                Fill_Array(out b);
+
+                InsertEvens(a, ref b);
+                Console.WriteLine("\nМассив A:");
+                Print_Array(a);
+                Console.WriteLine("\nРезультат добавления всех чётных элементов массива A в конец массива B");
+                Print_Array(b);
+
+                Console.WriteLine("\n\n-------------\nНажмите ESC для завершения программы.\nДля повтора нажмите любую другую клавишу.\n-------------");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        }
+
+        /// <summary>
+        /// Заполение массива целыми числами с клавиатуры
+        /// </summary>
+        /// <param name="a"></param>
+        public static void Fill_Array(out int[] a)
+        {
+            a = new int[100];
+            int temp; // очередной элемент массива
+            int k = 0; // номер элемента массива
+            Console.WriteLine("Введите очередной элемент массива - целое число. Для завершения ввода введите 0.");
+            do
+            {
+                while (!int.TryParse(Console.ReadLine(), out temp))
+                {
+                    Console.WriteLine("Неверный ввод");
+                    Console.WriteLine("Введите очередной элеент массива - целое число. Для завершения ввода введите 0.");
+                }
+                if (temp != 0)
+                {
+                    a[k++] = temp;
+                    if (k == a.Length)
+                    {
+                        Array.Resize(ref a, 2 * k);
+                    }
+                }
+            } while (temp != 0);
+            Array.Resize(ref a, k);
+        }
+
+        /// <summary>
+        /// Добавить все чётные элементы первого массива
+        /// в конец второго
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static public void InsertEvens(int[] a, ref int[] b)
+        {
+            if (a == null || a.Length == 0) return; // проверяем, что первый массив не пустой и не null
+
+            if (b == null) b = new int[0];
+
+            int k = b.Length; // длина второго массива. Динамически увеличивается
+            Array.Resize(ref b, a.Length + b.Length);
+
+            foreach (int elem in a) // дабавляем элементы из первого массива во второй массив
+            {
+                if (elem % 2 == 0) b[k++] = elem;
+            }
+            Array.Resize(ref b, k);
+
+        }
+
+        /// <summary>
+        /// Распечатать массив int[]
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void Print_Array(int[] arr)
+        {
+            if (arr == null) return; // проверяем, что массив не null
+
+            if (arr.Length == 0)
+            {
+                Console.WriteLine("[]");
+            }
+            else
+            {
+                Console.Write("[");
+                foreach (int elem in arr)
+                {
+                    Console.Write($"{elem}, ");
+                }
+                Console.Write("\b\b]\n");
+            }
+        }
+
+    }
+}
