d7f404b1ff0aa75919d4ed98d63d40ce907f8b4c
ifde <ifde@yandex.ru>
2023-10-09 09:08:20 +0300

    First

---
+++ Module 1/Week6/06aLastYear/ConsoleApp4/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Задача:      4
+Определить вспомогательный метод для выделения из символьного массива подмассива, 
+не содержащего конкретных символов, определяемых параметром типа string.  
+ Дата:      09.10.2023
+*/
+
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        /// <summary>
+        /// Dыделения из символьного массива подмассива, 
+        /// не содержащего конкретных символов, определяемых параметром типа string
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        static char[] Row(char[] line, string model)
+        {
+            char[] res = new char[line.Length]; // итоговый массив
+            int k = 0; // длина массива res 
+            foreach (char ch in line)
+            {
+                bool flag = true; //флаг
+                foreach (char restr in model)
+                {
+                    if (restr == ch)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag) res[k++] = ch;
+            }
+
+            return res;
+
+        }
+
+    }
+}
---
+++ Module 1/Week6/06aLastYear/ConsoleApp5/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Задача:      5
+Написать метод ConvertHex2Bin(), выполняющий перевод шестнадцатеричного числа в двоичное
+ Дата:      09.10.2023
+*/
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Шестнадцатеричное число: 5A1");
+            Console.WriteLine($"В бинарном формате: {ConvertHex2Bin("5A1")}");
+        }
+
+        /// <summary>
+        /// Перевод шестнадцатеричного числа в двоичное
+        /// </summary>
+        /// <param name="HexNumber"></param>
+        /// <returns></returns>
+        static string ConvertHex2Bin(string HexNumber)
+        {
+            int value = Convert.ToInt32(HexNumber, 16);
+            return Convert.ToString(value, 2);
+        }
+
+    }
+}
