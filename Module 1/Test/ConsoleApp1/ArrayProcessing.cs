4fb315e8956438785a3e25887925d6bb1b11f191
ifde <ifde@yandex.ru>
2023-10-21 10:09:13 +0300

    First

---
+++ Module 1/Test/ConsoleApp1/ArrayProcessing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ArrayProcessing
+    {
+        /// <summary>
+        /// Create an array consisting of N random integers from 10 to 65
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns>Created array</returns>
+        public static int[] CreateArray(int n)
+        {
+            int[] arr = new int[n]; // initialize an output array
+            Random rnd = new Random(); // random numbers generator
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(10, 66);
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Create a string from an Array. Separator - "; "
+        /// </summary>
+        /// <param name="ar"></param>
+        /// <returns>Created string. </returns>
+        public static string AsString(int[] ar)
+        {
+            string res = ""; // method output
+            if (ar == null || ar.Length == 0) return res; // return an empty string if ar[] contains no elements
+            return string.Join("; ", ar);
+        }
+
+        public static void MergeArray(int[] a, int[] b, out int[] c)
+        {
+            a ??= new int[0]; // if a[] is null, create an empty array
+            b ??= new int[0]; // if b[] is null, create an empty array
+            c = new int[Math.Max(a.Length, b.Length)];
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i >= a.Length) c[i] = 0;
+                    else c[i] = a[i];
+                }
+                else
+                {
+                    if (i >= b.Length) c[i] = 0;
+                    else c[i] = b[i];
+                }
+            }
+        }
+    }
+}
---
+++ Module 1/Test/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Вариант:     XXX
+ Дата:       21.10.2023
+*/
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // повтор решения 
+            do
+            {
+                Console.Clear();
+
+                int[][] c;
+                int n;
+                // input N
+                Console.WriteLine("Введите целое число N (0 < N < 7)");
+                while (!int.TryParse(Console.ReadLine(), out n) || n <= 0 || n >= 7)
+                {
+                    Console.WriteLine("Wrong input.");
+                    Console.WriteLine("Введите целое число N (0 < N < 7)");
+                }
+                c = new int[n][];
+
+                for (int i = 0; i < n; i++)
+                {
+                    int k, m;
+                    // input K
+                    Console.WriteLine("\nВведите целое число K (0 <= K < 15)");
+                    while (!int.TryParse(Console.ReadLine(), out k) || k < 0 || k >= 15)
+                    {
+                        Console.WriteLine("Wrong input.");
+                        Console.WriteLine("Введите целое число K (0 <= K < 15)");
+                    }
+
+                    // input M
+                    Console.WriteLine("\nВведите целое число M (0 <= M < 15)");
+                    while (!int.TryParse(Console.ReadLine(), out m) || m < 0 || m >= 15)
+                    {
+                        Console.WriteLine("Wrong input.");
+                        Console.WriteLine("Введите целое число M (0 <= M < 15)");
+                    }
+
+                    int[] a = ArrayProcessing.CreateArray(k);
+                    Console.WriteLine($"\nМассив A: {ArrayProcessing.AsString(a)}");
+
+                    int[] b = ArrayProcessing.CreateArray(m);
+                    Console.WriteLine($"\nМассив B: {ArrayProcessing.AsString(b)}");
+
+                    ArrayProcessing.MergeArray(a, b, out c[i]);
+                    Console.WriteLine($"\nC[{i}]: {ArrayProcessing.AsString(c[i])}");
+                }
+
+                Console.WriteLine("\n\n-------------\nНажмите ESC для завершения программы.\nДля повтора нажмите любую другую клавишу.\n-------------");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        static void InputInt(out int a, int left, int right, string name)
+        {
+            Console.WriteLine($"\nВведите целое число {name} ({left} <= {name} < {right})");
+            while (!int.TryParse(Console.ReadLine(), out a) || a < 0 || a >= 15)
+            {
+                Console.WriteLine("Wrong input.");
+                Console.WriteLine("Введите целое число K (0 <= K < 15)");
+            }
+        }
+    }
+}
