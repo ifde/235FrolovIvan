c5ce75b1a9d03d3b0976e50948fe906133530c85
ifde <ifde@yandex.ru>
2023-11-03 09:18:54 +0300

    First

---
+++ Module 2/Homework01/CSV_ClassLibrary/CsvProcessing.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CSV_ClassLibrary
+{
+    public static class CsvProcessing
+    {
+        static string fPath = @"..\..\..\transport-changing-points.csv";
+
+        /// <summary>
+        /// Returns an array(string[]) from a file at fPath.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string[] Read()
+        {
+            // cheking if the file exists
+            if (!File.Exists(fPath)) throw new ArgumentNullException();
+
+            string[] lines = File.ReadAllLines(fPath); // reading the CSV file
+
+            // checking if the header is correct
+            if (lines[0] != "\"ID\";\"TPUName\";\"global_id\";\"AdmArea\";" +
+                "\"District\";\"NearStation\";\"YearOfComissioning\";\"Status\";" +
+                "\"AvailableTransfer\";\"CarCapacity\";\"geodata_center\";\"geoarea\";")
+            {
+                throw new ArgumentNullException();
+            }
+
+            // cheking that each line is in correct format
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string str = "\"96\";\"Транспортно-пересадочный узел «Сортировочная» (п)\";\"1769845\";\"Юго-Восточный административный округ\";\"район Лефортово\";\"платформа «Сортировочная»\";\"2014\";\"проект\";\"пригородная железная дорога\";\"\";\"\";\"\";\r\n";
+                MatchCollection matches = Regex.Matches(lines[i], @"""[^""]*"";");
+                if (matches.Count != 12) throw new ArgumentNullException();
+
+            }
+
+            return lines;
+        }
+    }
+}
---
+++ Module 2/Homework01/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Задача:      КДЗ-1 (2 модуль)
+ Дата:       03.11.23
+*/
+using CSV_ClassLibrary;
+
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = CsvProcessing.Read();
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
