414ace6b4608e154542644b992e74f7838d1f803
ifde <ifde@yandex.ru>
2023-11-11 09:13:43 +0300

    First

---
+++ Module 2/Week 2/02aLastYear/ConsoleApp4/ArithmeticSequence.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class ArithmeticSequence
+    {
+        private double _start; // the first value of a sequence
+        private double _increment; // difference
+
+        public ArithmeticSequence()
+        {
+            _start = 0;
+            _increment = 1;
+        }
+
+        public ArithmeticSequence(double start, double increment)
+        {
+            _start = start;
+            _increment = increment;
+        }
+
+        public double this[int index]
+        {
+            get {  return _start + _increment * (index - 1); }
+        }
+
+        public override string ToString()
+        {
+            return $"a1 = {_start}, d = {_increment}";
+        }
+
+        /// <summary>
+        /// Calculates the sum of the frist n elements of a sequence.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public double GetSum(int n)
+        {
+            if (n <= 0) throw new Exception("Количество элементов должно быть натуральным числом.");
+            return n * (_start + _increment * (n - 1) / 2);
+        }
+    }
+}
---
+++ Module 2/Week 2/02aLastYear/ConsoleApp4/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Задача:      4
+Дата:        11.11.23
+*/
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // повтор решения 
+            do
+            {
+                Console.Clear();
+
+                try
+                {
+                    ArithmeticSequence sequence; // a separate sequence
+                    Random rnd = new Random();
+                    ArithmeticSequence[] arr = new ArithmeticSequence[rnd.Next(5, 16)];
+
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        int start = rnd.Next(1001);
+                        int increment = rnd.Next(1, 11);
+
+                        arr[i] = new ArithmeticSequence(start, increment);
+                    }
+
+                    sequence = new ArithmeticSequence(rnd.Next(1001), rnd.Next(1, 11));
+
+                    int step = rnd.Next(3, 16); // index
+
+                    int cnt = 1; // counter
+                    foreach (ArithmeticSequence elem in arr)
+                    {
+                        Console.WriteLine($"Последовательность {cnt++}:" +
+                            $"\nСумма первых {step} членов - {elem.GetSum(step)}");
+                        if (elem[step] > sequence[step]) Console.WriteLine(elem);
+                        Console.WriteLine(Environment.NewLine);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+
+                Console.WriteLine("\n\n-------------\nНажмите ESC для завершения программы.\nДля повтора нажмите любую другую клавишу.\n-------------");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+        }
+    }
+}
