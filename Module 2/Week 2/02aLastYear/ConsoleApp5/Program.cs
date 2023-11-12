f69c589bf6bc3fdd295b42584010533d216dafd5
ifde <ifde@yandex.ru>
2023-11-12 08:30:22 +0300

    First

---
+++ Module 2/Week 2/02aLastYear/ConsoleApp5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            // a separate videofile
+            VideoFile myfile = new VideoFile(GenerateString(rnd.Next(2, 10)), 
+                rnd.Next(60, 361), rnd.Next(100, 1001));
+            Console.WriteLine("Отдельный видеофайл:\n" + myfile + "\n");
+
+
+            // an array of videofiles
+            VideoFile[] arr = new VideoFile[rnd.Next(5, 16)];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new VideoFile(GenerateString(rnd.Next(2, 10)), rnd.Next(60, 361), rnd.Next(100, 1001));
+                if (arr[i].Size > myfile.Size) Console.WriteLine(arr[i] + "\n");
+            }
+
+        }
+
+        /// <summary>
+        /// Generates a string of latin symbols of a given size
+        /// </summary>
+        /// <param name="legnth"></param>
+        /// <returns></returns>
+        static string GenerateString(int length)
+        {
+            if (length <= 0) return "";
+            Random rnd = new Random();
+            char[] temp = new char[length]; // an array of symbols
+            for (int i = 0; i < length; i++)
+            {
+                temp[i] = (char)('a' + rnd.Next('z' - 'a' + 1));
+            }
+            return new string(temp);
+        }
+    }
+}
---
+++ Module 2/Week 2/02aLastYear/ConsoleApp5/VideoFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class VideoFile
+    {
+        private string _name; // videofile's name
+        private int _duration; // duration in seconds
+        private int _quality; // the quality of the videofile
+
+        public VideoFile(string name, int duration, int quality)
+        {
+            _name = name;
+            _duration = duration;
+            _quality = quality;
+        }
+
+        /// <summary>
+        /// The size of the videofile
+        /// </summary>
+        public int Size => _duration * _quality;
+
+        public override string ToString()
+        {
+            return $"Name = {_name}, Duration = {_duration}, Quality = {_quality}";
+        }
+    }
+}
