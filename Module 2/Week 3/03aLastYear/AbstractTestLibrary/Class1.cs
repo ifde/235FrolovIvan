dbc5ea8d20af371f890f12e7ad974f08bceaf2b5
ifde <ifde@yandex.ru>
2023-11-16 09:12:04 +0300

    first

---
+++ Module 2/Week 3/03aLastYear/AbstractTestLibrary/Class1.cs
@@ -0,0 +1,82 @@
+﻿namespace AbstractTestLibrary
+{
+    // Абстрактный класс UnitBase.
+    // Определяет набор полей для классов наследников:
+    abstract public class UnitBase
+    {
+        protected int unit_code; // номер единицы
+        protected double price; // цена за единицу
+        protected string name; // название
+
+        public int Unit_code
+        {
+            get => unit_code;
+            set => unit_code = value;
+        }
+
+        public double Price
+        {
+            get => price;
+            set => price = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary>
+        /// Calculates the discounted price
+        /// </summary>
+        /// <param name="discount"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        virtual public double Discount(int discount)
+        {
+            if (discount <= 0 || discount > 100) { throw new ArgumentException("Значение скидки указано неверно"); }
+            return price * (1 - (double)discount / 100);
+        }
+    }
+
+    // Book - класс определяет книгу. 
+    // Наследует  абстрактный класс UnitBase
+    public class Book : UnitBase
+    {
+        private int no_of_pages; // количество страниц в книге
+        private bool _isBestSeller; // является ли бестселлером
+
+        public Book(int no_of_pages, bool isBestSeller)
+        {
+            this.no_of_pages = no_of_pages;
+            _isBestSeller = isBestSeller;
+        }
+    }
+
+    // Card - класс определяет карточки.
+    // Наследует абстрактный класс UnitBase
+    public class Card : UnitBase
+    {
+        private string message; // сообщение
+
+        public Card(string message)
+        {
+            this.message = message;
+        }
+    }
+
+    // CD - класс определяет лазерный диск. 
+    // Наследует абстрактный класс UnitBase
+    public class CD : UnitBase
+    {
+        private int playing_time; // время звучания диска
+
+        public CD(int playing_time)
+        {
+            this.playing_time = playing_time;
+        }
+    }
+
+
+
+}
---
+++ Module 2/Week 3/03aLastYear/ConsoleApp5/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Дисциплина: "Программирование"
+Группа:      БПИ235(2)
+Студент:     Фролов Иван Григорьевич
+Задача:      5
+ Дата:       16.11.2023
+*/
+
+using AbstractTestLibrary;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            UnitBase[] arr = new UnitBase[30];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int object_number = rand.Next(3); // defines which object to create
+
+                if (object_number == 0) arr[i] = new Book(rand.Next(1, 101),
+                    rand.Next(2) == 0 ? false : true);
+
+                else if (object_number == 1) arr[i] = new Card($"Message {rand.Next(10)}");
+
+                else arr[i] = new CD(rand.Next(1, 150));
+
+                arr[i].Unit_code = i;
+                arr[i].Name = new string((char)('a' + rand.Next('z' - 'a' + 1)), rand.Next(1, 11));
+                arr[i].Price = rand.Next(100, 1001);
+            }
+
+            int discount;
+            Console.WriteLine("Введите значение скидки в процентах:");
+            while (!int.TryParse(Console.ReadLine(), out discount) || discount <= 0 || discount > 100)
+            {
+                Console.WriteLine("Неверный ввод. Повторите попытку.");
+            }
+            Console.WriteLine(Environment.NewLine);
+
+            foreach (UnitBase elem in arr)
+            {
+                Console.WriteLine($"Item {elem.Name}: Full price = {elem.Price}, Discounted price = {elem.Discount(discount)}");
+            }
+        }
+    }
+}
