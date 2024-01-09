5f30d156a24deff528081a3d3652a84d219b0321
ifde <ifde@yandex.ru>
2024-01-09 07:42:19 +0300

    first

---
+++ Module 3/3.05aLastYear/Task05Lib/Queue.cs
@@ -0,0 +1,26 @@
+﻿namespace Task05Lib
+{
+    public class Queue<T>
+    {
+        List<T> list = new List<T>();
+        public void Enqueue(T obj)
+        {
+            list.Add(obj);
+        }
+
+        public T Dequeue()
+        {
+            if (list.Count == 0) throw new ApplicationException("The queue is empty!");
+            T temp = list[0];
+            list.RemoveAt(0);
+            return temp;
+        }
+
+        public T First => IsEmpty ? throw new ApplicationException("The queue is empty!") : list[0];
+        public T Last => IsEmpty ? throw new ApplicationException("The queue is empty!") : list[list.Count - 1];
+
+        public bool IsEmpty => list.Count == 0 ? false : true;
+
+        public int Capacity => list.Count;
+    }
+}
