10c181459ca3efe4b064e1822cdb2528014af6a4
ifde <ifde@yandex.ru>
2024-01-22 09:56:16 +0300

    First

---
+++ Module 3/3.06aLastYear/Task03/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task03
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            using (FileStream inFi =
+               new FileStream(@"..\..\..\Program.cs", FileMode.Open))
+            {
+                int t;      // числовое значение прочитанного байта
+                while ((t = inFi.ReadByte()) != -1)
+                {
+                    if (t >= '0' && t <= '9')
+                        Console.WriteLine(t + " - " + (char)t + " - " + inFi.Position);
+                }   // while
+            }     // using
+        }   // Main() 
+
+    }
+}
