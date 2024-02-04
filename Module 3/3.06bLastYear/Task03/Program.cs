c5992fe00ba6280f413ef5cf1498b3b9f232aff6
ifde <ifde@yandex.ru>
2024-02-04 11:01:06 +0300

    first

---
+++ Module 3/3.06bLastYear/Task03/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.Design;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Xml;
+using Task03Lib;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // повтор решения 
+            do
+            {
+                Console.Clear();
+
+                try
+                {
+                    List<MyColor> colors = new List<MyColor>();
+                    using (StreamReader sr = new StreamReader(@"../../../css-color-names.json"))
+                    {
+                        string? line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (line == "{" || line == "}") continue;
+                            string color_name = "";
+                            foreach (Match match in Regex.Matches(line, @"(?<="")[^"":]*(?="")"))
+                            {
+                                string value = match.Value;
+                                if (value[0] != '#') color_name = value; // reading color_name
+                                else
+                                {
+                                    colors.Add(new MyColor(color_name, MyColor.StringToColor(value)));
+                                }
+                            }
+                        }
+                    }
+
+                    foreach (var color in  colors)
+                    {
+
+                        Console.ForegroundColor = FromColor(color.GetColor);
+                        Console.WriteLine(color.Name);
+                    }
+                    Console.ResetColor();
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.ParamName);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Возникла непредвиденная ошибка.");
+                }
+
+
+                Console.WriteLine("\n\n-------------\nНажмите ESC для завершения программы.\nДля повтора нажмите любую другую клавишу.\n-------------");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        static ConsoleColor FromColor(Color c)
+        {
+            int index = (c.R > 128 | c.G > 128 | c.B > 128) ? 8 : 0; // Bright bit
+            index |= (c.R > 64) ? 4 : 0; // Red bit
+            index |= (c.G > 64) ? 2 : 0; // Green bit
+            index |= (c.B > 64) ? 1 : 0; // Blue bit
+            return (ConsoleColor)index;
+        }
+    }
+}
---
+++ Module 3/3.06bLastYear/Task03Lib/Classes.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+namespace Task03Lib
+{
+    public class MyColor
+    {
+        string name;
+        byte r, g, b;
+
+        public string Name => name;
+        public Color GetColor => Color.FromArgb(r, g, b);
+
+        public MyColor(string name, byte[] bytes)
+        {
+            this.name = name;
+            r = bytes[0]; 
+            g = bytes[1]; 
+            b = bytes[2];
+        }
+
+        /// <summary>
+        /// Converts a color in "#f0f8ff" format to an array of R, G, B bytes
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static byte[] StringToColor(string str)
+        {
+            if (str.Length != 7) throw new ArgumentException();
+            byte[] bytes = new byte[3];
+            string r = str[1..3];
+            string g = str[3..5];
+            string b = str[5..7];
+
+            Color temp = Color.FromArgb(Convert.ToInt32(r, 16), Convert.ToInt32(g, 16), Convert.ToInt32(b, 16));
+            bytes[0] = temp.R;
+            bytes[1] = temp.G;
+            bytes[2] = temp.B;
+
+            return bytes;
+        }
+    }
+}
