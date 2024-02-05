ce38bc499febf3c30ca033fdd839edb1d6c544c2
ifde <ifde@yandex.ru>
2024-02-05 08:50:12 +0300

    first

---
+++ Module 3/3.06bLastYear/Task07/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Security.Cryptography;
+using Task07Lib;
+
+namespace Task07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Student[]));
+
+            Student[] students1 = new Student[3] { new Student("A"), new Student("B"), new Student("C") };
+
+            using (FileStream fs = new FileStream(@"../../../MyStudents.json", FileMode.Create))
+            {
+                ser.WriteObject(fs, students1);
+            }
+
+            DataContractJsonSerializer ser1 = new DataContractJsonSerializer(typeof(List<Student>));
+            using (FileStream fs = new FileStream(@"../../../Students.json", FileMode.Open))
+            {
+                List<Student> students = ser1.ReadObject(fs) as List<Student> ?? new List<Student>();
+                Console.WriteLine(students.Count);
+
+                if (students.Count == 0) Console.WriteLine("The list is empty!");
+                foreach (Student student in students)
+                {
+                    Type t = student.GetType();
+                    foreach (var field in t.GetFields(BindingFlags.Public |
+                                              BindingFlags.NonPublic |
+                                              BindingFlags.Instance))
+                    {
+                        Console.WriteLine($"{field.Name}: {field.GetValue(student)}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Module 3/3.06bLastYear/Task07Lib/Classes.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace Task07Lib
+{
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public string _id;
+        [DataMember]
+        public int age;
+        [DataMember]
+        public string eyeColor;
+        [DataMember]
+        public string name;
+        [DataMember]
+        public string gender;
+        [DataMember]
+        public int studentGroup;
+        [DataMember]
+        public int algebra;
+        [DataMember]
+        public int programming;
+        [DataMember]
+        public int english;
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+    }
+}
