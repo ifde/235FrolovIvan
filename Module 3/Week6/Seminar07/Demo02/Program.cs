85f3c7ee1bf2435996a583a05ab093014b84f596
ifde <ifde@yandex.ru>
2024-02-15 08:00:58 +0300

    first

---
+++ Module 3/Week6/Seminar07/Demo02/Program.cs
@@ -0,0 +1,27 @@
+﻿using Demo02Lib;
+
+namespace Demo02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 7 };
+
+            Sequence<int> even = new Sequence<int>(arr);
+
+            even[2] = 15;// Изменение по индексу.
+            try
+            {
+                // Изменение по несуществующему индексу.
+                even[-4] = 10;
+                // Создание последовательности с пустой ссылкой.
+                Sequence<double> x = new Sequence<double>(null);
+            }
+            catch (IndexOutOfRangeException e) { Console.WriteLine("Элемент с таким индексом отсутствует"); }
+            catch (ArgumentNullException ex) { Console.WriteLine("Ссылка null"); }
+            catch (Exception) { Console.WriteLine("Что-то еще случилось"); }
+
+        }
+    }
+}
---
+++ Module 3/Week6/Seminar07/Demo02Lib/Sequence.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Demo02Lib
+{
+    public class Sequence<T>
+    {
+        T[] _seq;
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 || i  >= _seq.Length) throw new IndexOutOfRangeException();
+                return _seq[i];
+            }
+
+            set { _seq[i] = value; }
+        }
+
+        public Sequence(T[] seq)
+        {
+            if (seq == null) throw new ArgumentNullException(nameof(seq));
+            _seq = seq;
+        }
+
+        public Sequence() : this(new T[0]) { }
+    }
+}
