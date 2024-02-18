b24f6ca195536f87a100c79e9899621884adf865
ifde <ifde@yandex.ru>
2024-02-18 08:26:09 +0300

    first

--- Module 3/Week6/Seminar07/Demo04/Program.cs
+++ Module 3/Week6/Seminar07/Demo04/Program.cs
@@ -9,6 +9,24 @@ namespace Demo04
             Point point1 = new Point(1, 1);
             Point point2 = new Point(2, 2);
 
+            Point[] points = new Point[10];
+            Random rnd = new Random();
+            Console.WriteLine("Initial array:");
+            for (int i = 0; i < 10; i++)
+            {
+                points[i] = new Point(rnd.Next(10), rnd.Next(10));
+                Console.WriteLine(points[i]);
+            }
+            Console.WriteLine();
+
+            Array.Sort(points);
+
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(points[i]);
+            }
+
             Pair<Point> pp = new Pair<Point>(point2, point1);
             Pair<Point> pp2 = new Pair<Point>(point1, point2);
 
--- Module 3/Week6/Seminar07/Demo04Lib/PointClasses.cs
+++ Module 3/Week6/Seminar07/Demo04Lib/PointClasses.cs
@@ -5,7 +5,7 @@ namespace Demo04Lib
     {
         int CompareTo(T other);
     }
-    public class Point: IComparable<Point>
+    public class Point : IComparable<Point>
     {
         public double X { get; }
         public double Y { get; }
@@ -16,25 +16,33 @@ namespace Demo04Lib
             Y = y;
         }
 
-        private double DistanceToZero()
-        {
-            return Math.Sqrt(X * X + Y * Y);
-        }
+        private double DistanceToZero => Math.Sqrt(X * X + Y * Y);
 
         public int CompareTo(Point? other)
         {
             if (other == null) throw new ArgumentNullException();
-            return (int)(DistanceToZero() - other.DistanceToZero());
+            return (int)(DistanceToZero - other.DistanceToZero);
         }
 
         public override string ToString()
         {
             return $"({X}, {Y})";
         }
+
+        public static bool operator >(Point point1, Point point2)
+        {
+            if (point1.DistanceToZero - point2.DistanceToZero < 0) return false;
+            return true;
+        }
+
+        public static bool operator <(Point point1, Point point2)
+        {
+            return point2 > point1;
+        }
     }
 
     public class Pair<T>
-        where T: IComparable<T>
+        where T : IComparable<T>
     {
         T t1, t2;
         public Pair(T t1, T t2)
@@ -43,7 +51,7 @@ namespace Demo04Lib
             {
                 this.t1 = t2;
                 this.t2 = t1;
-            } 
+            }
             else
             {
                 this.t1 = t1;
