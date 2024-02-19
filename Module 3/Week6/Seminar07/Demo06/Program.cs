d0a5aa7c92d676ea419872f08643dcb98a13f0ca
ifde <ifde@yandex.ru>
2024-02-19 07:49:16 +0300

    first

---
+++ Module 3/Week6/Seminar07/Demo06/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Demo06
+{
+    public enum Answers
+    {
+        Yes,
+        No,
+        Maybe,
+        Most_Definitely,
+        Unknown,
+        Unclear,
+        ItDepends
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Input your question");
+                string question = Console.ReadLine() + "";
+                Console.WriteLine(GetRandomAnswer(new Answers()));
+
+                Console.WriteLine("\n\n--------\nESC to kill the program. Any other key to continue.\n--------\n");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            
+
+        }
+
+        static string GetRandomAnswer(Answers answers)
+        {
+            Random rnd  = new Random();
+            int number = rnd.Next(Enum.GetNames(answers.GetType()).Length);
+            string? value = Enum.GetName(answers.GetType(), number);
+
+            if (value == null) throw new NullReferenceException();
+            return value;
+        }
+    }
+}
