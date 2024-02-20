c768a1450a72bbbee6be9fd74d759d45ff0a3a6b
ifde <ifde@yandex.ru>
2024-02-20 07:47:54 +0300

    first

---
+++ Module 3/Week6/Seminar07/Self01Lib/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Self01Lib
+{
+    public class BinaryTree<BTNode>
+    {
+        T root;
+        public BinaryTree() { }
+
+        public void Insert(BTNode node)
+        {
+
+        }
+    }
+
+    public class BTNode<T>
+    {
+        T node;
+    }
+}
