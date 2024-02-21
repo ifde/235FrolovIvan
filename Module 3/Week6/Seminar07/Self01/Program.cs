32efbfdb04a5164e250fe8bae179a472cacf3f86
ifde <ifde@yandex.ru>
2024-02-21 08:57:34 +0300

    first

--- Module 3/Week6/Seminar07/Self01/Program.cs
+++ Module 3/Week6/Seminar07/Self01/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Self01
+﻿using Self01Lib;
+
+namespace Self01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BinaryTree<BTNode<int>, int> tree = new BinaryTree<BTNode<int>, int>();
+            BTNode<int> node1 = new BTNode<int>(1);
+            BTNode<int> node2 = new BTNode<int>(2);
+            BTNode<int> node3 = new BTNode<int>(3);
+            tree.Insert(node1);
+            node1.InsertValue(node2);
+            node1.InsertValue(node3);
+            Console.WriteLine(tree);
         }
     }
 }
--- Module 3/Week6/Seminar07/Self01Lib/Class1.cs
+++ Module 3/Week6/Seminar07/Self01Lib/Class1.cs
@@ -1,18 +1,68 @@
 ﻿namespace Self01Lib
 {
-    public class BinaryTree<BTNode>
+    public class BinaryTree<BTNode, T>
+        where T: notnull
     {
-        T root;
-        public BinaryTree() { }
+        BTNode<T> root;
+        public BinaryTree() { root = new BTNode<T>(default); }
 
-        public void Insert(BTNode node)
+        public void Insert(BTNode<T> newNode)
         {
+            if (newNode.LeftLink != null) throw new ArgumentException("Узел не является частью дерева");
+            root.InsertValue(newNode);
 
         }
+
+        public string Preorder(BTNode<T> node, ref string res)
+        {
+            if (node.LeftLink == null && node != root) throw new ArgumentException("Узел не является частью дерева");
+            foreach(var childNode in node.RightLink)
+            {
+                if (childNode.RightLink.Count == 0) res += $"|{childNode.Value}|\n";
+                else Preorder(childNode, ref res);
+            }
+            res += $"|{node.Value}|\n";
+            return res;
+        }
+
+        public override string ToString()
+        {
+            if (root.RightLink.Count == 0) throw new ArgumentNullException("Дерево пустое.");
+            string res = "";
+            return Preorder(root, ref res);
+        }
+
+        public bool IsEmpty => root == null;
     }
 
     public class BTNode<T>
+        where T: notnull
     {
-        T node;
+        T value;
+        int cnt;
+        BTNode<T>? leftLink;
+        List<BTNode<T>> rightLink;
+
+        public BTNode<T>? LeftLink => leftLink;
+        public List<BTNode<T>> RightLink => rightLink;
+
+        public T Value => value;
+        public int Cnt => cnt;
+
+        public BTNode(T value)
+        {
+            this.value = value;
+            leftLink = null;
+            rightLink = new List<BTNode<T>>();
+            cnt = 0;
+        }
+
+        public void InsertValue(BTNode<T> newNode)
+        {
+            if (newNode.leftLink != null) throw new ArgumentException("Узел уже является частью дерева.");
+            newNode.leftLink = this;
+            rightLink.Add(newNode);
+            cnt++;
+        }
     }
 }
