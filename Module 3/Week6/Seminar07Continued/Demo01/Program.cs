7ec2cdc6168d7f10f4256b2a3584070433bf7271
ifde <ifde@yandex.ru>
2024-02-23 09:23:45 +0300

    first

--- Module 3/Week6/Seminar07Continued/Demo01/Program.cs
+++ Module 3/Week6/Seminar07Continued/Demo01/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Demo01
+﻿using Demo01Lib;
+
+namespace Demo01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SideOfTheWorldEncoder sideOfTheWorldEncoder = new SideOfTheWorldEncoder();
+            WindEncoder windEncoder = new WindEncoder();
+
+            byte[] encoding1 = sideOfTheWorldEncoder.Encode("С");
+            Console.WriteLine(ByteArrayToString(encoding1));
+            Console.WriteLine(sideOfTheWorldEncoder.Decode(encoding1));
+
+            byte[] encoding2 = windEncoder.Encode("СЗ");
+            Console.WriteLine(ByteArrayToString(encoding2));
+            Console.WriteLine(windEncoder.Decode(encoding2));
+
+        }
+
+        static string ByteArrayToString(byte[] arr)
+        {
+            string res = "";
+            foreach (byte b in arr) res += b;
+            return res;
         }
     }
 }
--- Module 3/Week6/Seminar07Continued/Demo01Lib/Classes.cs
+++ Module 3/Week6/Seminar07Continued/Demo01Lib/Classes.cs
@@ -14,26 +14,34 @@ namespace Demo01Lib
 
         public byte[] Encode(string u)
         {
-            Dictionary<string, byte[]> dict = new Dictionary<string, byte[]>();
+            Dictionary<string, byte[]> dict = GetDictionary();
+            return dict[u];
             byte[] cipher = new byte[u.Length];
             for (int i = 0; i < u.Length; i++)
             {
                 cipher[i] = (byte)((u[i] - 'A' + key) % 26);
             }
             return cipher;
-            // return GetDictionary().
         }
 
         public string Decode(byte[] t)
         {
-            // TODO 02: реализовать данный метод
-            string res = "";
-            foreach (byte b in t)
+            Dictionary<string, byte[]> dict = GetDictionary();
+            foreach (var key in dict.Keys)
             {
-                res += (b - 5) % 26 + 'A';
+                if (CompareByteArrays(dict[key], t)) return key;
             }
-            return res;
-            // return GetDictionary().
+            throw new ArgumentException("Не удалость расшифровать.");
+        }
+
+        private bool CompareByteArrays(byte[] arr1, byte[] arr2)
+        {
+            if (arr1.Length != arr2.Length) return false;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != arr2[i]) return false;
+            }
+            return true;
         }
     }
 
@@ -50,4 +58,23 @@ namespace Demo01Lib
             };
         }
     }
+
+    public class WindEncoder : BinaryToStringEncoder
+    {
+        protected override Dictionary<string, byte[]> GetDictionary()
+        {
+            return new Dictionary<string, byte[]>
+            {
+                { "С", new byte[] { 0, 0, 0 } },
+                { "Ю", new byte[] { 1, 0, 0 } },
+                { "З", new byte[] { 1, 1, 0 } },
+                { "В", new byte[] { 0, 1, 0 } },
+                { "СЗ", new byte[] { 1, 1, 1 } },
+                { "СВ", new byte[] { 0, 0, 1 } },
+                { "ЮЗ", new byte[] { 1, 0, 1 } },
+                { "ЮВ", new byte[] { 0, 1, 1 } }
+            };
+        }
+    }
+
 }
