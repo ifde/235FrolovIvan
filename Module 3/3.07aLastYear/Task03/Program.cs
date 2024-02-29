f89a23622d5ddefe217d2cbfa30e8ceba2fc8bc7
ifde <ifde@yandex.ru>
2024-02-29 08:03:43 +0300

    first

---
+++ Module 3/3.07aLastYear/Task03/Program.cs
@@ -0,0 +1,44 @@
+﻿using Task03Lib;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input M:");
+            int m;
+
+            int.TryParse(Console.ReadLine(), out m);
+            int[] sum = new int[m];
+            Fibbonacci fi = new Fibbonacci();
+            TriangleNums triangles = new TriangleNums();
+
+            int i = 0;
+            foreach (int numb in fi.GetSequence(m))
+            {
+                Console.Write(numb + "  ");
+                sum[i++] = numb;
+            }
+                
+            Console.WriteLine();
+            i = 0;
+            foreach (int numb in triangles.GetTriangleNums(m))
+            {
+                Console.Write(numb + "  ");
+                sum[i++] += numb;
+            }
+                
+            Console.WriteLine();
+
+            Console.WriteLine("The sum:");
+            foreach (int elem in sum)
+            {
+                Console.Write(elem + "  ");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ Module 3/3.07aLastYear/Task03Lib/Fibbonacci.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace Task03Lib
+{
+    public class Fibbonacci
+    {
+        int last, current = 1;
+
+        public IEnumerable<int> GetSequence(int cnt)
+        {
+            if (cnt <= 0) throw new ArgumentException("Negative numbers are not supported.");
+            for (int i = 0; i < cnt; i++)
+            {
+                int t = current;
+                current = last + current;
+                last = t;
+                yield return current;
+            }
+        }
+    }
+
+    public class TriangleNums
+    {
+        int n = 0;
+        public IEnumerable<int> GetTriangleNums(int cnt)
+        {
+            for (int i = 0; i < cnt; i++)
+            {
+                yield return n * (n  + 1) / 2;
+                n++;
+            }
+        }
+    }
+}
