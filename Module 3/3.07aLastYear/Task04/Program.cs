fd85bc2755982dca2a544c053c60e58105bc9920
ifde <ifde@yandex.ru>
2024-03-01 08:05:38 +0300

    first

---
+++ Module 3/3.07aLastYear/Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Headers;
+using Task04Lib;
+
+namespace Task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Evens ev = new Evens(1, 25);
+            Primes pr = new Primes(-100, 100);
+            foreach (int item in ev) Console.Write(item + " ");
+            Console.WriteLine();
+            foreach (int item in pr) Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Module 3/3.07aLastYear/Task04Lib/Classes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace Task04Lib
+{
+    public class Evens
+    {
+        int nMin, nMax;
+        public Evens (int min, int max)
+        {
+            if (min > max) throw new ArgumentException();
+            nMin = min;
+            nMax = max;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = nMin; i <= nMax; i++)
+            {
+                if (i % 2 == 0) yield return i;
+            }
+        }
+    }
+
+    public class Primes
+    {
+        int nMin, nMax;
+        public Primes(int min, int max)
+        {
+            if (min > max) throw new ArgumentException();
+            nMin = min;
+            nMax = max;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = nMin; i <= nMax; i++)
+            {
+                if (IsPrime(i)) yield return i;
+            }
+        }
+
+        private bool IsPrime(int n)
+        {
+            if (n == 1 || n == -1) return false;
+            for (int i = 2;  i < (n > 0 ? n : -n) / 2; i++)
+            {
+                if (n % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
