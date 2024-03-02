af9b8f63c92608ff0a749877b61b88e4ffae38aa
ifde <ifde@yandex.ru>
2024-03-02 07:52:11 +0300

    first

---
+++ Module 3/3.07aLastYear/Task05/Program.cs
@@ -0,0 +1,24 @@
+﻿using Task05Lib;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6 };
+            MyIterator<int> collect_3 = new MyIterator<int>(values, 3);
+            foreach (object ob in collect_3)
+                Console.Write(ob + "  ");
+            Console.WriteLine();
+            foreach (int ob in new MyIterator<int>(values, 1))
+                Console.Write(ob + "  ");
+            Console.WriteLine();
+            foreach (int ob in new BFIterator<int>(values))
+                Console.Write(ob + "  ");
+            Console.WriteLine();
+            Console.WriteLine("\nНажмите ENTER. . . ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module 3/3.07aLastYear/Task05Lib/Iterators.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace Task05Lib
+{
+    public class MyIterator<T> : IEnumerable<T>
+    {
+        T[] collection;
+        int start;
+        public MyIterator(T[] collection, int start)
+        {
+            if (start >= collection.Length || start < 0) throw new ArgumentException();
+            this.collection = collection;
+            this.start = start;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                yield return collection[(i + start) % collection.Length];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class BFIterator<T> : IEnumerable<T>
+    {
+        T[] collection;
+        public BFIterator(T[] collection)
+        {
+            this.collection = collection;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                yield return collection[i];
+            }
+            for (int i = collection.Length - 1;  i >= 0; i--)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
