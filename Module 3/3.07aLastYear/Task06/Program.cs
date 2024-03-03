0682278f860588e2cdc138fb28241888e1a75a31
ifde <ifde@yandex.ru>
2024-03-03 08:54:10 +0300

    firs

---
+++ Module 3/3.07aLastYear/Task06/Program.cs
@@ -0,0 +1,20 @@
+﻿using Task06Lib;
+
+namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileLines source = new FileLines(@"..\..\..\Program.cs");
+
+            foreach (string item in source)
+                Console.WriteLine(item);
+            Console.WriteLine("*******************************************");
+
+            foreach (string tem in source)
+                Console.WriteLine(tem);
+
+        }
+    }
+}
---
+++ Module 3/3.07aLastYear/Task06Lib/Files.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace Task06Lib
+{
+    public class FileLines : IEnumerable<string>, IDisposable
+    {
+        string fileName;
+        StreamReader fileStream;
+
+        public FileLines(string fileName)
+        {
+            this.fileName = fileName;
+            fileStream = new StreamReader(fileName);
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new FileEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new FileEnumerator(this);
+        }
+
+        class FileEnumerator : IEnumerator<string>
+        {
+            string? line;
+            FileLines fl;
+            public FileEnumerator(FileLines fl)
+            {
+                this.fl = fl;
+                line = null;
+            }
+            public string Current => line;
+            object IEnumerator.Current => line;
+            public bool MoveNext()
+            {
+                line = fl.fileStream.ReadLine();
+                if (line == null)
+                {
+                    Reset();
+                    return false;
+                }
+                return true;
+            }
+            public void Reset()
+            {
+                fl.fileStream.Close();
+                fl.fileStream = new StreamReader(fl.fileName);
+            }
+
+            public void Dispose()
+            {
+                
+            }
+        }
+
+        public void Dispose()
+        {
+            fileStream.Dispose();
+        }
+    }
+}
