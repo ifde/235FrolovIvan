79acbd59105b394871b063c08ef4933d01b193fb
ifde <ifde@yandex.ru>
2024-03-05 17:09:10 +0300

    first

---
+++ Module 3/3.07bLastYear/Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Маша", "Вася", "Наташа", "Петя", "Ира", "Даня" };
+            Array.ForEach(names, Console.WriteLine);
+            Console.WriteLine("======================= Manual: ");
+            
+            IEnumerable<string> namesWithSh = from name in names
+                                              where name.Contains("ш") && name.Contains("а")
+                                              orderby name descending
+                                              select name;
+            foreach (string name in namesWithSh)
+            {
+                Console.Write(name + " ");
+            }
+
+        }
+    }
+}
---
+++ Module 3/3.07bLastYear/Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task02
+{
+    internal class Program
+    {
+        public static uint FigSum(uint k)
+        {
+            if (k / 10 == 0) return k;
+            return FigSum(k / 10) + k % 10;
+        }
+        static void PrintSeries<T>(IEnumerable<T> ser)
+        {
+            var line = ser.Select(x => x.ToString());
+            Console.WriteLine(Enumerable.Aggregate(line, (x, y) => x + "\t" + y));
+        }
+        static void Main(string[] args)
+        {
+            uint[] row = { 543, 67, 234, 765 };
+            Console.WriteLine("Исходный массив:");
+            PrintSeries(row);
+            Console.WriteLine("Суммы цифр элементов массива: ");
+            var series = row.Select(x => FigSum(x));
+            PrintSeries(series);
+            var res = series.Sum(x => x);
+            Console.WriteLine("Общая сумма цифр = " + res);
+        }
+    }
+}
