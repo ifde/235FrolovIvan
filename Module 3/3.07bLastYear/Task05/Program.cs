8a2427e0b8a8101cf31717dc4977d13fa7fa5697
ifde <ifde@yandex.ru>
2024-03-08 08:17:24 +0300

    First

---
+++ Module 3/3.07bLastYear/Task05/Program.cs
@@ -0,0 +1,45 @@
+﻿using Task05Lib;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] test = { "one", "two", "three", "four", "five", "zero" };
+            Words words = new Words(test);
+            Console.WriteLine("Названия цифр по алфавиту:");
+            foreach (var item in words)
+                foreach (var el in item)
+                    Console.Write(el + "  ");
+            Console.WriteLine();
+
+            // Выберем из поcледовательности массив слов на букву 'f':
+            var res = from s in words
+                      where s.Length > 0 && s[0][0] == 'f'
+                      select s;
+
+            Console.WriteLine("Названия цифр на букву 'f':");
+            foreach (var item in res)
+                foreach (var el in item)
+                    Console.WriteLine(el);
+
+            Words keys = new Words(MyLib.keywords);
+            int cnt = 0;
+            foreach (var key in keys) cnt++;
+            Console.WriteLine("Number of elements in keys[]:" + cnt);
+            Console.WriteLine("This is because keys[] includes empty arrays.");
+
+            // Выберем из поcледовательности массив слов на букву 'i':
+            var set = from s in keys
+                      where s.Length > 0 && s[0][0] == 'i'
+                      select s;
+
+            Console.WriteLine("Служебные слова на букву 'i':");
+            foreach (var item in set)
+                foreach (var el in item)
+                    Console.WriteLine(el);
+
+        }
+    }
+}
---
+++ Module 3/3.07bLastYear/Task05Lib/Classes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace Task05Lib
+{
+    public class Words : IEnumerable<string[]>
+    {
+        string[] source; // Ссылка на исходный массив «слов»
+        public Words(string[] source)
+        {     // Конструктор
+            this.source = source;
+        }
+
+        public IEnumerator<string[]> GetEnumerator()
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < letters.Length; i++)
+            {
+                string[] resAr = (from word in source
+                                  where letters[i] == word[0]
+                                  select word).ToArray();
+                yield return resAr;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        { return GetEnumerator(); }
+    }
+
+    public class MyLib
+    {
+        static public string[] keywords = {
+"abstract", "as", "base", "bool", "break",
+"byte", "case", "catch", "char", "checked",
+"class", "const", "continue", "decimal", "default",
+"delegate", "do", "double", "else", "enum",
+"event", "explicit", "extern", "false", "finally",
+"fixed", "float", "for", "foreach", "goto",
+"if", "implicit", "in", "int", "interface",
+"internal", "is", "lock", "long", "namespace",
+"new", "null", "object", "operator", "out",
+"override", "params", "private", "protected", "public",
+"readonly", "ref", "return", "sbyte", "sealed",
+"short", "sizeof", "stackalloc", "static", "string",
+"struct", "switch", "this", "throw", "true",
+"try", "typeof", "uint", "ulong", "unchecked",
+"unsafe", "ushort", "using", "virtual", "void",
+"volatile", "while"  };  //  string[ ]
+    }   //  class MyLib
+
+}
