b5af2fc4353d88d412073df9b9aa4058223e0aa0
ifde <ifde@yandex.ru>
2024-03-09 08:23:43 +0300

    First

---
+++ Module 3/3.07bLastYear/Task06/Program.cs
@@ -0,0 +1,38 @@
+﻿using Task06Lib;
+using System.Linq;
+
+namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rainbow colors = new Rainbow();
+            Console.WriteLine("Исходная последовательность:");
+            foreach (string color in colors)
+                Console.Write(color);
+
+            Console.WriteLine("\r\nВыборка коротких слов:");
+            var u = colors.Where(x => x.Length <= 6);
+
+            foreach (string color in u)
+                Console.Write(color);
+
+            Console.WriteLine("\r\nСлова упорядочены по длинам:");
+            IEnumerable<string> seq = colors.OrderBy(x => x.Length);
+
+            foreach (string color in seq)
+                Console.Write(color);
+
+            Console.WriteLine("\r\nСлова упорядочены по алфавиту:");
+            IEnumerable<string> pre = colors.OrderBy(x => x.ToString());
+
+            foreach (string color in pre)
+                Console.Write(color);
+
+            Console.WriteLine("\r\nДля выхода нажмите ENTER...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Module 3/3.07bLastYear/Task06Lib/Classes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Task06Lib
+{
+    public class Rainbow : IEnumerable<string>
+    {
+        public IEnumerator<string>
+            GetEnumerator()
+        {
+            yield return "каждый ";
+            yield return "охотник ";
+            yield return "желает ";
+            yield return "знать ";
+            yield return "где ";
+            yield return "сидит ";
+            yield return "фазан ";
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
