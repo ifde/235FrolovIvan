742e303dfb790965d07557c7095c705eda10fd45
ifde <ifde@yandex.ru>
2024-03-10 08:06:43 +0300

    first

---
+++ Module 3/3.07bLastYear/Task07/Program.cs
@@ -0,0 +1,42 @@
+﻿using Task07Lib;
+
+namespace Task07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PointList set = new PointList();
+            set.Add(new Point(4, 5));
+            set.Add(new Point(4, 5));
+            set.Add(new Point(4, 5));
+            set.Add(new Point(7, 1));
+            set.Add(new Point(7, 2));
+            set.Add(new Point(5, 2));
+            set.Add(new Point(7, 2));
+            Console.WriteLine("Список точек на плоскости:");
+            foreach (Point p in set)
+                Console.WriteLine(p.ToString());
+
+            // Обработка списка в LINQ-запросах:
+            IEnumerable<Point> newList =
+                from v in set
+                orderby v.Mod
+                select v;
+
+            Console.WriteLine("Список упорядоченных точек:");
+            foreach (Point p in newList)
+                Console.WriteLine(p.ToString());
+
+            var res = from r in set
+                      where r.Mod > 7
+                      select r;
+            //Console.WriteLine(res.GetType());
+            Console.WriteLine("Список точек c модулем большим 7:");
+            foreach (Point p in res)
+                Console.WriteLine(p.ToString());
+
+
+        }
+    }
+}
---
+++ Module 3/3.07bLastYear/Task07Lib/Classes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace Task07Lib
+{
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public double Mod => Math.Sqrt(X * X + Y * Y);
+
+        public bool Equals(Point other)
+        => X == other.X && Y == other.Y;
+
+        public override string ToString()
+        => $"x = {X}, y = {Y}, mod = {Mod:G5}";
+    }
+
+    public class PointList : IEnumerable<Point>
+    {
+        private List<Point> list = new();
+        public int Count { get { return list.Count; } }
+        // Индексатор для класса PointList:
+        public Point this[int index]
+        {
+            get => list[index];
+            set => list[index] = value;
+        }
+        // Добавлять в список только новые точки:
+        public void Add(Point point)
+        {
+            foreach (Point p in list)
+                if (p.Equals(point)) return;
+            list.Add(point);
+        }
+
+        // Реализуем члены интерфейсов:
+        public IEnumerator<Point> GetEnumerator()
+        {
+            foreach(Point p in list) yield return p;
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    } // PointList
+}
