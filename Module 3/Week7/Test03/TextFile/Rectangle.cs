a8ce767d4afe6ad76ea9fc67f678601bd4d4c236
ifde <ifde@yandex.ru>
2024-03-14 17:04:38 +0300

    First

---
+++ Module 3/Week7/Test03/TextFile/Rectangle.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+
+namespace TextFile
+{
+    public class Rectangle
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Rectangle(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Area => X * Y;
+    }
+
+    public class RectangleList
+    {
+        private Rectangle[] _rectangles;
+        private int[] indices;
+
+        public RectangleList(string fileName)
+        {
+            string path = @"../../../../data/fileName";
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                _rectangles = new Rectangle[lines.Length];
+                int cnt = 0;
+                foreach (string line in lines)
+                {
+                    // CultureInfo.CurrentCulture = new CultureInfo("en-US");
+                    double x, y;
+                    string[] sides = line.Split(' ');
+                    if (sides.Length != 2) continue;
+                    if (!double.TryParse(sides[0], out x)) continue;
+                    if (!double.TryParse(sides[1], out y)) continue;
+                    _rectangles[cnt++] = new Rectangle(x, y);
+                }
+                Array.Resize(ref _rectangles, cnt);
+            }
+            catch (Exception)
+            {
+                _rectangles = new Rectangle[0];
+                return;
+            }
+            finally
+            {
+                indices = new int[_rectangles.Length];
+                for (int i = 0; i < indices.Length; i++)
+                {
+                    indices[i] = i;
+                }
+            }
+            
+        }
+
+        public IEnumerable<Rectangle> GetIterator(int mode)
+        {
+            for (int i = 0; i < _rectangles.Length - 1; i++)
+            {
+                for (int j = i + 1; j < _rectangles.Length; j++)
+                {
+                    if (mode * _rectangles[i].Area > mode * _rectangles[j].Area)
+                    {
+                        int temp = indices[i];
+                        indices[i] = indices[j];
+                        indices[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                yield return _rectangles[i];
+            }
+        }
+    }
+}
