feb3c70f9f3b3f05d811ca1e4c04c85c3ab32c08
ifde <ifde@yandex.ru>
2024-03-15 20:16:51 +0300

    first

---
+++ Module 3/Week7/Test03/Main1/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Main1Lib;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // repeat program
+            do
+            {
+                Console.Clear();
+
+                try
+                {
+
+
+
+                    Console.WriteLine("Выберите имя файла для создания объекта EllipseList (без указания расширения)");
+                    string fileName;
+                    EllipseList ellipses;
+                    while (true)
+                    {
+                        fileName = Console.ReadLine() + "";
+                        ellipses = new EllipseList(fileName);
+                        if (ellipses.Ellipses.Length != 0) break;
+                        Console.WriteLine("Возникла ошибка.\nПовторите попытку.");
+                    }
+
+                    do
+                    {
+                        Console.WriteLine("Меню:\n1 - Сохранить неупорядоченные данные\n" +
+                        "2 - Сохранить упорядоченные по возрастанию меньшего диаметра данные + диапазон\n" +
+                        "3 - Сохранить упорядоченные по возрастанию большего диаметра  данные");
+                        int command;
+                        while (true)
+                        {
+                            if (int.TryParse(Console.ReadLine(), out command) && command >= 1 && command <= 3) break;
+                            Console.WriteLine("Wrong input. Try again.");
+                        }
+
+                        double minn = 0, maxx = 0;
+                        if (command == 2)
+                        {
+                            Console.WriteLine("Введите MIN:");
+                            while(true)
+                            {
+                                if (double.TryParse(Console.ReadLine(), out minn) && minn > 0) break;
+                                Console.WriteLine("Wrong input. Try again.");
+                            }
+
+                            Console.WriteLine("Введите MAX:");
+                            while (true)
+                            {
+                                if (double.TryParse(Console.ReadLine(), out maxx) && maxx > 0 && maxx >= minn) break;
+                                Console.WriteLine("Wrong input. Try again.");
+                            }
+                        }
+
+
+                        string path = @"../../../../input/output.txt";
+                        if (!File.Exists(path))
+                        {
+                            var fs = File.Create(path);
+                            fs.Close();
+                        }
+
+                        var enumerator = command switch
+                        {
+                            1 => ellipses.Ellipses.AsEnumerable(),
+                            2 => ellipses.GetIterator(1),
+                            3 => ellipses.GetIterator(-1),
+                            _ => throw new Exception()
+                        };
+
+                        string lines = "\n";
+                        foreach (var rectangle in enumerator)
+                        {
+                            if (command == 2)
+                            {
+                                if (rectangle.MaxDiametr < minn || rectangle.MaxDiametr > maxx) continue;
+                            }
+                            lines += rectangle.ToString() + '\n';
+                        }
+                        File.AppendAllText(path, lines);
+
+                        Console.WriteLine("Сохраненные данные:");
+                        Console.WriteLine(File.ReadAllText(path));
+
+                        Console.WriteLine("Press ENTER to go again.\nPress any other key to finish program.");
+                    }
+                    while (Console.ReadKey().Key == ConsoleKey.Enter);
+
+
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Возникла непредвиденная ошибка.");
+                }
+
+                Console.WriteLine("\n\n-------------\nНажмите ESC для завершения программы.\nДля повтора нажмите любую другую клавишу.\n-------------");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ Module 3/Week7/Test03/Main1Lib/EllipeClasses.cs
@@ -0,0 +1,103 @@
+﻿using System.Globalization;
+
+namespace Main1Lib
+{
+    public class Ellipse
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Ellipse(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double MinDiametr => Math.Min(X, Y);
+        public double MaxDiametr => Math.Max(X, Y);
+
+        public override string ToString()
+        {
+            return $"{X:f3} {Y:f3}";
+        }
+    }
+
+    public class EllipseList
+    {
+        private Ellipse[] _Ellipses;
+        private int[] indices;
+
+        public Ellipse[] Ellipses { get { return _Ellipses; } }
+
+        public EllipseList(string fileName)
+        {
+            string path = @"../../../../input/" + fileName + ".txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                _Ellipses = new Ellipse[lines.Length];
+                int cnt = 0;
+                foreach (string line in lines)
+                {
+                    // CultureInfo.CurrentCulture = new CultureInfo("en-US");
+                    double x, y;
+                    string[] sides = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    if (sides.Length != 2) continue;
+                    if (!double.TryParse(sides[0], out x)) continue;
+                    if (!double.TryParse(sides[1], out y)) continue;
+                    _Ellipses[cnt++] = new Ellipse(x, y);
+                }
+                Array.Resize(ref _Ellipses, cnt);
+            }
+            catch (Exception)
+            {
+                _Ellipses = new Ellipse[0];
+                return;
+            }
+            finally
+            {
+                indices = new int[_Ellipses.Length];
+                for (int i = 0; i < indices.Length; i++)
+                {
+                    indices[i] = i;
+                }
+            }
+
+        }
+
+        public IEnumerable<Ellipse> GetIterator(int mode)
+        {
+            for (int i = 0; i < _Ellipses.Length - 1; i++)
+            {
+                for (int j = i + 1; j < _Ellipses.Length; j++)
+                {
+                    if (mode == 1)
+                    {
+                        if (_Ellipses[indices[i]].MinDiametr > _Ellipses[indices[j]].MinDiametr)
+                        {
+                            int temp = indices[i];
+                            indices[i] = indices[j];
+                            indices[j] = temp;
+                        }
+                    }
+                    else
+                    {
+                        if (_Ellipses[indices[i]].MaxDiametr > _Ellipses[indices[j]].MaxDiametr)
+                        {
+                            int temp = indices[i];
+                            indices[i] = indices[j];
+                            indices[j] = temp;
+                        }
+                    }
+                    
+                }
+            }
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                yield return _Ellipses[indices[i]];
+            }
+        }
+    }
+}
