13a696554d6650289986400bd21e8c94048b0c07
ifde <ifde@yandex.ru>
2024-03-15 07:58:56 +0300

    first

--- Module 3/Week7/Test03/Main/Program.cs
+++ Module 3/Week7/Test03/Main/Program.cs
@@ -30,49 +30,58 @@ namespace Main
                         Console.WriteLine("Возникла ошибка.\nПовторите попытку.");
                     }
 
-                    Console.WriteLine("Меню:\n1 - Сохранить неупорядоченные данные\n" +
+                    do
+                    {
+                        Console.WriteLine("Меню:\n1 - Сохранить неупорядоченные данные\n" +
                         "2 - Сохранить упорядоченные по возрастанию данные\n" +
                         "3 - Сохранить упорядоченные по убыванию данные");
-                    int command;
-                    while (true)
-                    {
-                        if (int.TryParse(Console.ReadLine(), out command) && command >= 1 && command <= 3) break;
-                        Console.WriteLine("Wrong input. Try again.");
-                    }
+                        int command;
+                        while (true)
+                        {
+                            if (int.TryParse(Console.ReadLine(), out command) && command >= 1 && command <= 3) break;
+                            Console.WriteLine("Wrong input. Try again.");
+                        }
 
-                    List<int> numbers = new List<int>();
-                    foreach (string temp in Directory.EnumerateFiles(@"../../../../data/"))
-                    {
-                        var match = Regex.Match(temp, @"(?<=/output-)\d*(?=\.txt)");
-                        string value = match.Value;
-                        if (string.IsNullOrEmpty(value)) continue;
-                        int number;
-                        int.TryParse(value, out number);
-                        if (number != 0) numbers.Add(Convert.ToInt32(value));
+                        List<int> numbers = new List<int>();
+                        foreach (string temp in Directory.EnumerateFiles(@"../../../../data/"))
+                        {
+                            var match = Regex.Match(temp, @"(?<=/output-)\d*(?=\.txt)");
+                            string value = match.Value;
+                            if (string.IsNullOrEmpty(value)) continue;
+                            int number;
+                            int.TryParse(value, out number);
+                            if (number != 0) numbers.Add(number);
 
+                        }
+                        numbers.Sort();
+
+                        string path;
+                        if (numbers.Count == 0) path = @"../../../../data/output-1.txt";
+                        else path = @"../../../../data/output-" + (numbers.Last() + 1) + ".txt";
+
+                        var enumerator = command switch
+                        {
+                            1 => rectangles.Rectangles.AsEnumerable(),
+                            2 => rectangles.GetIterator(1),
+                            3 => rectangles.GetIterator(-1),
+                            _ => throw new Exception()
+                        };
+
+                        string lines = "";
+                        foreach (var rectangle in enumerator)
+                        {
+                            lines += rectangle.ToString() + '\n';
+                        }
+                        File.WriteAllText(path, lines);
+
+                        Console.WriteLine("Сохраненные данные:");
+                        Console.WriteLine(File.ReadAllText(path));
+
+                        Console.WriteLine("Press ENTER to go again.\nPress any other key to finish program.");
                     }
-                    numbers.Sort();
+                    while (Console.ReadKey().Key == ConsoleKey.Enter);
+
                     
-                    string path;
-                    if (numbers.Count == 0) path = @"../../../../data/output-1.txt";
-                    else path = @"../../../../data/output-" + (numbers.Last() + 1) + ".txt";
-
-                    var enumerator = command switch
-                    {
-                        1 => rectangles.Rectangles.AsEnumerable<Rectangle>(),
-                        2 => rectangles.GetIterator(1),
-                        3 => rectangles.GetIterator(-1),
-                        _ => throw new Exception()
-                    };
-
-                    string lines = "";
-                    foreach (var rectangle in enumerator)
-                    {
-                        lines += rectangle.ToString() + '\n';
-                    }
-                    File.WriteAllText(path, lines);
-
-                    Console.WriteLine(File.ReadAllText(path));
                 }
                 catch (ArgumentException e)
                 {
